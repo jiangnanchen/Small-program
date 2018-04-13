ebdf3c3227d99d0eea80389d31a431f89a54f271
明日之星 <514418695@qq.com>
2018-04-14 00:20:38 +0800

    add 2018年4月14日

--- Myfrist/pages/index/index.js
+++ Myfrist/pages/index/index.js
@@ -4,59 +4,49 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-
-     iconSize: [20, 30, 40, 50, 60, 70],
-    iconColor: [
-      'red', 'orange', 'yellow', 'green', 'rgb(0,255,255)', 'blue', 'purple'
+    imgUrls: [
+      '/images/01.jpg',
+      '/images/02.jpg',
+      '/images/03.jpg',
+      '/images/04.jpg',
     ],
-    iconType: [
-      'success', 'success_no_circle', 'info', 'warn', 'waiting', 'cancel', 'download', 'search', 'clear'
-    ]
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+    indicatorDots: false,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000,
+    proList:[
+      {
+        logo: '/images/李白4.png',
+        title: '静夜思   --李白',
+        desc: '床前明月光，疑是地上霜。\n' +
+        '举头望明月，低头思故乡。',
+      },
+      {
+        logo: '/images/李白1.png',
+        title: '黄鹤楼送孟浩然之广陵 --李白',
+        desc: '故人西辞黄鹤楼，烟花三月下扬州。\n' +
+        '孤帆远影碧空尽，唯见长江天际流。',
+      },
+      {
+        logo: '/images/李白.png',
+        title: '望庐山瀑布 --李白',
+        desc: '日照香炉生紫烟，遥看瀑布挂前川。\n' +
+        '飞流直下三千尺，疑是银河落九天。',
+      },
+      {
+        logo: '/images/李白3.png',
+        title: '赠汪伦   --李白',
+        desc: '李白乘舟将欲行，忽闻岸上踏歌声。\n桃花潭水深千尺，不及汪伦送我情。',
+      },
+      {
+        logo: '/images/李白2.png',
+        title: '早发白帝城 --李白',
+        desc: '朝辞白帝彩云间，千里江陵一日还。\n两岸猿声啼不住，轻舟已过万重山。',
+      },
+
+    ],
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+  
   }
 })
